5993075d7f9b719273b2b13e242ce5857fe4c984
aranyuth <aranyuth@gmail.com>
2018-12-15 17:07:21 +0700

    fix array.php

--- array.php
+++ array.php
@@ -1,8 +1,8 @@
 <?php 
-$a=array ("Beer","Bom","Wat");
+$a=array ("Beer","Bom","Wat", "hello");
 $b=array_slice($a,1,2);
 foreach ($b as $value)
 {
-   echo $value."<br>\n";// แสดงออกมา Bom Wat 
+   echo "print".$value."<br>\n";// แสดงออกมา Bom Wat 
 }
 ?>
